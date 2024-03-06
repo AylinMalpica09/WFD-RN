3f53ad5c9f8d0ef5b9176510486be74a026bb26c
AylinMalpica09 <100156363+AylinMalpica09@users.noreply.github.com>
2024-03-06 15:34:12 -0600

    first version

---
+++ a.py
@@ -0,0 +1,22 @@
+from keras.models import load_model
+import cv2
+import numpy as np
+
+model = load_model('modelo.h5')
+
+image_path = input("por favor, ingrese la ruta de la imagen: ")
+
+image_path = image_path.strip('"')
+
+img =cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+img = cv2.resize(img, (64,64))
+img = np.array(img).reshape(-1, 64, 64, 1)
+
+#HACER LA PREDICCION 
+prediction = model.predict(img)
+
+#OBTENER LA CLASE PREDICHA
+classes =['fresa','mango']
+predicted_classes = classes[np.argmax(prediction)]
+
+print(f'La clase predicha es: {predicted_classes}')
---
+++ prueba.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+from keras.models import load_model
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+model = load_model('modelo.h5')
+classes =['fresa','mango']
+
+def process_image(image):
+    img = cv2.imdecode(np.frombuffer(image.read(), np.uint8), cv2.IMREAD_GRAYSCALE)
+    img = cv2.resize(img, (64, 64))
+    img = np.array(img).reshape(-1, 64, 64, 1)
+    prediction = model.predict(img)
+    predicted_class = classes[np.argmax(prediction)]
+    return predicted_class
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return render_template('index.html', message='No file part')
+        file = request.files['file']
+        if file.filename == '':
+            return render_template('index.html', message='No selected file')
+        if file:
+            predicted_class = process_image(file)
+            return render_template('result.html', predicted_class=predicted_class)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
